7d749cf89652ccb6bf90ca3feff5d64b200c9247
Niklas Pettersson <niklas@c-10-253-193-229.clients.eduroam.liu.se>
2018-09-06 16:59:06 +0200

    genesis(Pyramid): First commit of started project

---
+++ Pyramid TD/Assets/Scripts/Monster.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Monster : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+    // Navigate to Castle
+    GameObject pyramid = GameObject.Find("Pyramid");
+    if (pyramid)
+      GetComponent<UnityEngine.AI.NavMeshAgent>().destination = pyramid.transform.position;
+	}
+
+	void OnTriggerEnter(Collider co) {
+    // If castle then deal Damage
+    if (co.name == "Pyramid") {
+      co.GetComponentInChildren<Health>().decrease();
+			Destroy(gameObject);
+    }
+	}
+}
